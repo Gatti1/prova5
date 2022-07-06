aa99bd52377e3e1b7900ef50e7c1cf478ca264d7
Diego Moraes dos Santos Silva <diego.moraes@cadmus.com.br>
2022-07-06 13:07:15 -0300

    Adicionar arquivos de projeto.

---
+++ prova5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i < 100; i++)
+
+            {
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Numero par: " + i);
+                }
+            }
+        }
+    }
+}
